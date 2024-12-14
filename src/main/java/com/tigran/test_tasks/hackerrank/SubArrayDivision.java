5364953076272e46ef7d52fe3958f3fbfda6edda
TigranMelkonyan <tigran.melqonyan96@gmail.com>
2024-12-14 14:49:53 +0400

    Added task.

--- src/main/java/com/tigran/test_tasks/hackerrank/SubArrayDivision.java
+++ src/main/java/com/tigran/test_tasks/hackerrank/SubArrayDivision.java
@@ -10,7 +10,8 @@ import java.util.List;
  */
 public class SubArrayDivision {
     public static void main(String[] args) {
-        System.out.println(foo(Arrays.asList(1, 2, 3, 5, 9, 7, 3, 1), 4, 3));
+        System.out.println(foo(Arrays.asList(1, 2, 3, 4), 9, 3));
+        System.out.println(foo(Arrays.asList(1, 2, 3, 1, 2, 7, 3), 6, 3));
         System.out.println(foo(Arrays.asList(1, 1, 1, 1, 1, 1), 3, 2));
         System.out.println(foo(Arrays.asList(1, 2, 1, 3, 2), 3, 2));
         System.out.println(foo(Arrays.asList(4), 4, 1));
@@ -22,15 +23,29 @@ public class SubArrayDivision {
         int sum = 0;
 
         for (int i = 0; i < n; i++) {
-            sum += s.get(i); // Add the current element to the sum
-            if (i >= m - 1) { // When we have a complete window
+            sum += s.get(i);
+            if (i >= m - 1) {
                 if (sum == d) {
                     count++;
                 }
-                sum -= s.get(i - m + 1); // Slide the window by removing the first element
+                sum -= s.get(i - m + 1);
             }
         }
         return count;
     }
+
+//    static int foo(List<Integer> s, int d, int m) {
+//        int n = s.size();
+//        int count = 0;
+//        for (int i = 0; i < n - m + 1; i++) {
+//            int sum = 0;
+//            for (int j = 0; j < m; j++)
+//                sum = sum + s.get(i + j);
+//            if (sum == d) {
+//                count++;
+//            }
+//        }
+//        return count;
+//    }
 
 }
