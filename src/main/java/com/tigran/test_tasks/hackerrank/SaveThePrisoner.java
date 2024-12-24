201dc637ce98168cac1f07b69471a7b78dedb0fe
TigranMelkonyan <tigran.melqonyan96@gmail.com>
2024-12-24 17:56:01 +0400

    Added task.

---
+++ src/main/java/com/tigran/test_tasks/hackerrank/SaveThePrisoner.java
@@ -0,0 +1,19 @@
+package com.tigran.test_tasks.hackerrank;
+
+/**
+ * Created by Tigran Melkonyan
+ * Date: 12/24/24
+ * Time: 5:17 PM
+ */
+public class SaveThePrisoner {
+    public static void main(String[] args) {
+        System.out.println(saveThePrisoner(5, 2, 1));
+        System.out.println(saveThePrisoner(5, 2, 2));
+        System.out.println(saveThePrisoner(7, 19, 2));
+    }
+
+    public static int saveThePrisoner(int n, int c, int pos) {
+        return ((pos - 1 + c - 1) % n) + 1;
+    }
+
+}
