462945b226181665368190b11c65d1678bc02cd2
TigranMelkonyan <tigran.melqonyan96@gmail.com>
2024-12-28 15:13:14 +0400

    Added task.

--- src/main/java/com/tigran/test_tasks/hackerrank/SequenceEquation.java
+++ src/main/java/com/tigran/test_tasks/hackerrank/SequenceEquation.java
@@ -27,4 +27,21 @@ public class SequenceEquation {
         return res;
     }
 
+//    public static List<Integer> permutationEquation(List<Integer> p) {
+//        // Precompute the index map
+//        Map<Integer, Integer> indexMap = new HashMap<>();
+//        for (int i = 0; i < p.size(); i++) {
+//            indexMap.put(p.get(i), i + 1); // Map value to its position (1-based index)
+//        }
+//
+//        // Compute the result
+//        List<Integer> res = new ArrayList<>();
+//        for (int i = 1; i <= p.size(); i++) {
+//            int r = indexMap.get(indexMap.get(i));
+//            res.add(r);
+//        }
+//
+//        return res;
+//    }
+
 }
