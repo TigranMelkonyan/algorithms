72ba32155882c84f720872a6956023d76918b33c
TigranMelkonyan <tigran.melqonyan96@gmail.com>
2025-01-14 15:30:48 +0400

    Added task.

--- src/main/java/com/tigran/test_tasks/hackerrank/FairRations.java
+++ src/main/java/com/tigran/test_tasks/hackerrank/FairRations.java
@@ -20,10 +20,10 @@ public class FairRations {
     public static String fairRations(List<Integer> B) {
         int count = 0;
 
-        for (int i = B.size() - 1; i > 0; i--) {
+        for (int i = 0; i < B.size() - 1; i++) {
             if (B.get(i) % 2 != 0) {
                 B.set(i, B.get(i) + 1);
-                B.set(i - 1, B.get(i - 1) + 1);
+                B.set(i + 1, B.get(i + 1) + 1);
                 count += 2;
             }
         }
